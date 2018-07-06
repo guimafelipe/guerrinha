e9b5f2442a2a3f6685a8c010ee00f1aae11757e4
Felipe Guimaraes <guima.felipec@gmail.com>
2018-07-06 19:57:44 -0300

    added import definitions and will start to work on front end UI

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import EnemyUI from './EnemyUI';
+import GameScreen from './GameScreen';
+import PlayerUI from './PlayerUI';
+import ButtonsGroup from './ButtonsGroup';
 
 class Game extends Component {
     constructor (socket) {
@@ -13,6 +17,17 @@ class Game extends Component {
     }
     render (){
         return (
+            //Adversario ui (nome, balas, vidas)
+            //<EnemyUI/>
+
+            //Tela do jogo
+            //Ação do player x ação do adversário
+            
+            //Player ui (balas e vidas)
+
+            //Botões de ação (carregar defender atacar (bazuca?))
+
+
             <p>Aqui aparece coisas do gameplay </p>
         )
     }
