fea8c60295c1b259b8832a94a2af0ce77f1ec836
Felipe Guimaraes <guima.felipec@gmail.com>
2018-07-16 21:19:00 -0300

    made blueprint work

--- client/src/components/ButtonsGroup.js
+++ client/src/components/ButtonsGroup.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import Button from './Button';
+// import Button from './Button';
+import {Button, Spinner, Intent} from '@blueprintjs/core';
 
 class ButtonsGroup extends Component {
     render (){
         return (
             <div>
-                <Button handle={this.props.shotHandle} name={"Shot"}/>
+                {/* <Button handle={this.props.shotHandle} name={"Shot"}/>
                 <Button handle={this.props.reloadHandle} name={"Reload"}/>
                 <Button handle={this.props.shieldHandle} name={"Shield"}/>
-                <Button handle={this.props.quitHandle} name={"Quit"}/>
+                <Button handle={this.props.quitHandle} name={"Quit"}/> */}
+                <Button intent="success" text="shot" onClick={this.props.shotHandle}/>
+                <Spinner intent={Intent.PRIMARY} />;
             </div>
         );
     }
